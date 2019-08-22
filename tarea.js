a0d614e39b09cc57b74fe79233f5b87f58b3e00a
Yael Diaz <ydiaz@soldai.com>
2019-08-22 09:29:02 -0500

    revision del trabajo

--- tarea.js
+++ tarea.js
@@ -16,19 +16,19 @@ let outputYCbCr = rgb2YCbCr(input);
 cv.imwrite('YCbCr.jpg', outputYCbCr)
 
 //guardo las imagenes que me genera openCV
-
  outputGray = input.cvtColor(cv.COLOR_RGB2GRAY)
 // outputRGBN = input.normalize(1, 0, cv.NORM_MINMAX, -1)//no se como usarla
  outputYCbCr = input.cvtColor(cv.COLOR_RGB2YCrCb)
  cv.imwrite('YCbCr2.jpg', outputYCbCr);
  cv.imwrite('gray2.jpg', outputGray);
 // cv.imwrite('normalized2.jpg', outputRGBN);
+
 function rgb2Gray(image) {
   let output = new cv.Mat(image.sizes[0], image.sizes[1], cv.CV_8UC1)
   for (let i = 0; i < image.sizes[0]; i++) {
     for (let k = 0; k < image.sizes[1]; k++) {
-      let pixel = image.atRaw(i, k);
-      let sum = pixel[0] + pixel[1] + pixel[2];
+      let [b,g,r] = image.atRaw(i, k);
+      let sum = r+g+b;
       let gray = Math.round(sum / 3);
       output.set(i, k, gray)
     }
@@ -39,13 +39,13 @@ function rgb2RgbN(image) {
   let output = new cv.Mat(image.sizes[0], image.sizes[1], cv.CV_8UC3)
   for (let i = 0; i < image.sizes[0]; i++) {
     for (let k = 0; k < image.sizes[1]; k++) {
-      let pixel = image.atRaw(i, k);
-      let sum = pixel[0] + pixel[1] + pixel[2];
+      let [b,g,r] = image.atRaw(i, k);
+      let sum = r+g+b;
       let normalized = [0, 0, 0];
       if (sum !== 0) {
-        normalized = [Math.round(pixel[0] / sum * 255),
-        Math.round(pixel[1] / sum * 255),
-        Math.round(pixel[2] / sum * 255)
+        normalized = [Math.round(b / sum * 255),
+        Math.round(g / sum * 255),
+        Math.round(r / sum * 255)
       ]
     }
     output.set(i, k, normalized)
@@ -60,7 +60,6 @@ function rgb2YCbCr(image){
     [0.500, -0.419, -0.081]
   ];
 let output = new cv.Mat(image.sizes[0], image.sizes[1], cv.CV_8UC3)
-
   for (let i = 0; i < image.sizes[0]; i++) {
     for (let k = 0; k < image.sizes[1]; k++) {
       // retorna en formato B G R
@@ -71,13 +70,6 @@ let output = new cv.Mat(image.sizes[0], image.sizes[1], cv.CV_8UC3)
         if (j >0) {
            YCbCr[j] += 128;
         }
-      
-        // if (YCbCr[j] > 255) {
-        //   YCbCr[j] = 255;
-        // }
-        // if (YCbCr[j] < 0) {
-        //   YCbCr[j] = 0;
-        // }
       }
       output.set(i, k, YCbCr)
     }
